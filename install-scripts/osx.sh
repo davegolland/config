d6a7f9fb67cc126ef4f468818bcd06fc57c3aad2
davegolland <dave@getprismatic.com>
2016-01-30 14:22:10 -0800

    add install scripts

---
+++ install-scripts/osx.sh
@@ -0,0 +1,8 @@
+
+# Install Homebrew and various deps
+ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# install intellij
+brew install intellij-ce
